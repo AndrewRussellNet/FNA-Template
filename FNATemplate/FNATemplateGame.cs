684b8ca2557bfd576f9c0eb07465a71a28bbb162
Andrew Russell <andrew@andrewrussell.net>
2018-03-22 13:26:27 +1000

    Tidy up usings.

--- FNATemplate/FNATemplateGame.cs
+++ FNATemplate/FNATemplateGame.cs
@@ -1,8 +1,6 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
+﻿using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
--- FNATemplate/Input.cs
+++ FNATemplate/Input.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 
